6b1fafa004b9a0bfc474628198d0e14b1471bd27
Adarsh M <adarshadhu47167@gmail.com>
2025-10-02 00:00:43 +0530

    Best Time to Buy and Sell Stock

---
+++ array/solutions/best_time_to_buy&sell_stock.dart
@@ -0,0 +1,18 @@
+// 2. [Best Time to Buy and Sell Stock] find maximum profite
+import 'dart:math';
+
+List<int> prices = [7, 1, 5, 3, 6, 4];
+var minPrice = prices[0]; //lowest prixw
+
+void main() {
+  int profit = 0;
+  for (var i = 0; i < prices.length; i++) {
+    minPrice = min(minPrice, prices[i]);
+    profit = max(profit, prices[i] - minPrice);
+
+    print(
+      "Day $i -> minPrice: $minPrice, potential profit: ${prices[i] - minPrice}, maxProfit: $profit",
+    );
+  }
+  print(profit);
+}
--- array/solutions/two_sum.dart
+++ array/solutions/two_sum.dart
@@ -2,14 +2,15 @@
 // Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-final nums = [2,7,11,15];
-int target = 26;
+final nums = [1,2,3,4,5];
+int target = 9;
 Map<int,int> sampleMap= {};
 void main(){
   for (var i = 0; i < nums.length; i++) {
     var needed = target - nums[i];
     if (sampleMap.containsKey(needed)) {
       print('${sampleMap[needed]},$i');
+      return;
     }else{
       sampleMap[nums[i]] = i;
     }
